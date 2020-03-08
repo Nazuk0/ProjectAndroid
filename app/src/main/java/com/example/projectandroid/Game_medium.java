384dfd3f3705370933caef5e038eb54268f58038
Nicolas Thong <nicolas.thong@hotmail.com>
2020-03-08 19:29:14 +0100

    game init

--- app/src/main/java/com/example/projectandroid/Game_medium.java
+++ app/src/main/java/com/example/projectandroid/Game_medium.java
@@ -4,11 +4,41 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Game_medium extends AppCompatActivity {
+import java.util.Random;
+
+public class Game_hard extends AppCompatActivity {
+
+    int score=0;
+    Button item1
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_medium);
+    }
+
+    public String getRandomString(){
+        String[] array = getResources().getStringArray(R.array.words);
+        String randomStr = array[new Random().nextInt(array.length)];
+
+        return randomStr;
+    }
+
+    public void setTextBtn(){
+
+        item1 = findViewById(R.id.item1);
+        item1.setText(getRandomString());
+
+        item2 = findViewById(R.id.item2);
+        item2.setText(getRandomString());
+        while(item2.getText() == item1.getText()){
+            item2.setText(getRandomString());
+        }
+
+        item3 = findViewById(R.id.item3);
+        item3.setText(getRandomString());
+        while(item3.getText() == item1.getText() && item3.getText() == item2.getText()){
+            item2.setText(getRandomString());
+        }
     }
 }
---
+++ app/src/main/java/com/example/projectandroid/Game_medium_result.java
@@ -0,0 +1,14 @@
+package com.example.projectandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Game_medium_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_medium_result);
+    }
+}
